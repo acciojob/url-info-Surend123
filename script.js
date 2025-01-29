8230a3eb326e53400866a1f03f89833da4161c8f
Surendra <sky958447@gmail.com>
2025-01-29 19:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function lenthOfUrl(url){
+	return url.length;
+}
+const curr_url = window.location.href;
+alert(`The length of the URL is:${lengthOfUrl(curr_url)}`)
